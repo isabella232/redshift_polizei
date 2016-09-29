8eb79f2bc4c06f2e526a20f44911ee169615a967
Tobias Thiel <tobias011@gmail.com>
2016-09-29 14:23:23 +0200

    exports: update desmond for proper credentials forwarding & filter aws access denied emails

--- app/jobs/_helpers.rb
+++ app/jobs/_helpers.rb
@@ -4,6 +4,8 @@ module JobHelpers
       return true
     elsif type <= PG::SyntaxErrorOrAccessRuleViolation
       return true
+    elsif type <= Aws::S3::Errors::AccessDenied
+      return true
     end
     return false
   end
