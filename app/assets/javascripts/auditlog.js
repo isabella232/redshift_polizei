0b72fac0ae778ccfcf2b2a25736c164af67f132b
Tobias Thiel <tobias011@gmail.com>
2017-06-14 16:54:17 +0200

    added audit log date range filter

--- app/assets/javascripts/auditlog.js
+++ app/assets/javascripts/auditlog.js
@@ -24,4 +24,41 @@ $(document).ready(function() {
       }
     ]
   });
+
+  function init_datrange() {
+    var start  = moment($('.daterange input[name="start_date"]').val());
+    var end    = moment($('.daterange input[name="end_date"]').val());
+    var oldest = moment($('.daterange input[name="oldest_date"]').val());
+    var max_range = parseInt($('.daterange input[name="max_range"]').val());
+    var ranges = {
+      'Today': [moment(), moment()],
+      'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+      'Last 7 Days': [moment().subtract(7, 'days'), moment()]
+    };
+    ranges['Last ' + max_range + ' Days (All)'] = [oldest, moment()];
+
+    function cb(start, end) {
+        $('.daterange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+    }
+
+    $('.daterange').daterangepicker({
+        startDate: start,
+        endDate: end,
+        minDate: oldest,
+        maxDate: moment(),
+        opens: 'left',
+        'dateLimit': {
+          'days': max_range,
+        },
+        'linkedCalendars': false,
+        ranges: ranges,
+    }, cb);
+    $('.daterange').on('apply.daterangepicker', function(ev, picker) {
+      var start_date = picker.startDate.format('YYYY-MM-DD');
+      var end_date   = picker.endDate.format('YYYY-MM-DD');
+      window.location = '?start_date=' + start_date + '&end_date=' + end_date + '&selects=' + $('input[name="selects"]').val();
+    });
+    cb(start, end);
+  }
+  init_datrange();
 });
